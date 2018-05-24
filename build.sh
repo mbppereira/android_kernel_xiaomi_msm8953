fa947d51a16875591e6888f7bb642210b0cfa6ea
Mika-Pikazo <1376106928@qq.com>
2018-05-24 21:55:24 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+export CROSS_COMPILE=../aarch64-linux-android/bin/aarch64-opt-linux-android- && make ARCH=arm64 O=../out tiffany_defconfig && make ARCH=arm64 O=../out -j8 && cd ../out && cat arch/arm64/boot/Image.gz arch/arm64/boot/dts/qcom/*.dtb > zImage && mv zImage ~
